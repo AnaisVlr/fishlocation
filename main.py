dee80c3d9d8251d8ae3ffa39f38ebee0bbbf280f
Anaïs Velcker <anais.velcker@gmail.com>
2023-10-15 16:02:26 +0200

    Ajout fonctionalité trouver un poisson

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import pandas as pd
 
 df = pd.read_csv('premieres_1000_lignes.csv')
 
-df = df[['taxonID', 'scientificName','originalNameUsage','vernacularName','decimalLatitude', 'decimalLongitude', 'stateProvince', 'locality', 'eventDate']]
+#Traitement du csv pour ne garder que certaines colonnes qui nous sont utiles
+colonnes_a_conserver = ['taxonID', 'scientificName', 'vernacularName','decimalLatitude', 'decimalLongitude']
 
+nouveau_df = df[colonnes_a_conserver]
+
+#Création d'une liste de dictionnaires contenant la position des zones de prélèvement
 zones = []
-
 
 for index, row in df.iterrows():
     if 'decimalLatitude' in row and 'decimalLongitude' in row:
--- view.py
+++ view.py
@@ -3,7 +3,7 @@ import tkinter
 import tkinter.messagebox
 from tkintermapview import TkinterMapView
 
-from main import zones
+from main import zones, nouveau_df
 
 #Code from https://github.com/TomSchimansky/TkinterMapView/blob/main/examples/map_view_demo.py
 class App(tkinter.Tk):
@@ -19,7 +19,6 @@ class App(tkinter.Tk):
         self.geometry(f"{self.WIDTH}x{self.HEIGHT}")
 
         self.protocol("WM_DELETE_WINDOW", self.on_closing)
-        self.bind("<Return>", self.search)
 
         if sys.platform == "darwin":
             self.bind("<Command-q>", self.on_closing)
@@ -29,39 +28,33 @@ class App(tkinter.Tk):
         self.grid_columnconfigure(1, weight=0)
         self.grid_columnconfigure(2, weight=0)
         self.grid_rowconfigure(1, weight=1)
+        self.grid_rowconfigure(2, weight=1)
 
-        self.search_bar = tkinter.Entry(self, width=50)
-        self.search_bar.grid(row=0, column=0, pady=10, padx=10, sticky="we")
-        self.search_bar.focus()
+        #Search bar for position
+        self.search_position_bar = tkinter.Entry(self, width=50)
+        self.search_position_bar.grid(row=0, column=0, pady=10, padx=10, sticky="we")
+        self.search_position_bar.focus()
 
-        self.search_bar_button = tkinter.Button(master=self, width=8, text="Recherche Poisson", command=self.search)
-        self.search_bar_button.grid(row=0, column=1, pady=10, padx=10)
+        self.search_position_bar_button = tkinter.Button(master=self, width=8, text="Find position", command=self.search_position)
+        self.search_position_bar_button.grid(row=0, column=1, pady=10, padx=10)
 
-        self.search_bar_clear = tkinter.Button(master=self, width=8, text="Clear", command=self.clear)
-        self.search_bar_clear.grid(row=0, column=2, pady=10, padx=10)
+        self.search_position_bar_clear = tkinter.Button(master=self, width=8, text="Clear", command=self.clear_position)
+        self.search_position_bar_clear.grid(row=0, column=2, pady=10, padx=10)
 
+        #Search bar for fish
+        self.search_fish_bar = tkinter.Entry(self, width=50)
+        self.search_fish_bar.grid(row=1, column=0, pady=10, padx=10, sticky="we")
+        self.search_fish_bar.focus()
+
+        self.search_fish_bar_button = tkinter.Button(master=self, width=8, text="Find fish", command=self.search_fish)
+        self.search_fish_bar_button.grid(row=1, column=1, pady=10, padx=10)
+
+        self.search_fish_bar_clear = tkinter.Button(master=self, width=8, text="Clear", command=self.clear_fish)
+        self.search_fish_bar_clear.grid(row=1, column=2, pady=10, padx=10)
+
+        #Map
         self.map_widget = TkinterMapView(width=self.WIDTH, height=600, corner_radius=0)
-        self.map_widget.grid(row=1, column=0, columnspan=3, sticky="nsew")
-
-        self.marker_list_box = tkinter.Listbox(self, height=8)
-        self.marker_list_box.grid(row=2, column=0, columnspan=1, sticky="ew", padx=10, pady=10)
-
-        self.listbox_button_frame = tkinter.Frame(master=self)
-        self.listbox_button_frame.grid(row=2, column=1, sticky="nsew", columnspan=2)
-
-        self.listbox_button_frame.grid_columnconfigure(0, weight=1)
-
-        self.save_marker_button = tkinter.Button(master=self.listbox_button_frame, width=20, text="Sauvegarder marker",
-                                                 command=self.save_marker)
-        self.save_marker_button.grid(row=0, column=0, pady=10, padx=10)
-
-        self.clear_marker_button = tkinter.Button(master=self.listbox_button_frame, width=20, text="Supp liste de marker",
-                                                  command=self.clear_marker_list)
-        self.clear_marker_button.grid(row=1, column=0, pady=10, padx=10)
-
-        self.connect_marker_button = tkinter.Button(master=self.listbox_button_frame, width=20, text="connect marker with path",
-                                                    command=self.connect_marker)
-        self.connect_marker_button.grid(row=2, column=0, pady=10, padx=10)
+        self.map_widget.grid(row=2, column=0, columnspan=3, sticky="nsew")
 
         #self.map_widget.set_address("NYC")
         self.map_widget.set_position(43.66106,3.80563)
@@ -73,34 +66,51 @@ class App(tkinter.Tk):
 
         # Création des lieux de prélèvement
         for zone in zones:
-            self.map_widget.set_marker(zone["latitude"], zone["longitude"], text="")
+            self.marker_list.append(self.map_widget.set_marker(zone["latitude"], zone["longitude"], text=""))
 
 
-
-    def search(self, event=None):
+    def search_position(self, event=None):
+        self.clear_marker_list()
         if not self.search_in_progress:
             self.search_in_progress = True
             if self.search_marker not in self.marker_list:
                 self.map_widget.delete(self.search_marker)
 
-            address = self.search_bar.get()
+            address = self.search_position_bar.get()
             self.search_marker = self.map_widget.set_address(address, marker=True)
             if self.search_marker is False:
                 # address was invalid (return value is False)
                 self.search_marker = None
             self.search_in_progress = False
+    
+    
+    def search_fish(self, event=None):
+        self.clear_marker_list()
+        # Obtenir le nom du poisson recherché
+        poisson = self.search_fish_bar.get()
+        # Filtrer le dataframe pour ne garder que les lignes qui correspondent au poisson recherché
+        poissons_filtres = nouveau_df[nouveau_df['scientificName'] == poisson]
+        print(poissons_filtres)
+        # Obtenir les emplacements où le poisson a été trouvé
+        emplacements = poissons_filtres[['decimalLatitude', 'decimalLongitude']]
+
+        # Afficher les emplacements sur la carte
+        for _, row in emplacements.iterrows():
+            latitude = row['decimalLatitude']
+            longitude = row['decimalLongitude']
+            
+            self.marker_list.append(self.map_widget.set_marker(latitude, longitude, text=""))
+            self.map_widget.set_position(latitude, longitude)
+        
 
     def save_marker(self):
         if self.search_marker is not None:
-            self.marker_list_box.insert(tkinter.END, f" {len(self.marker_list)}. {self.search_marker.text} ")
-            self.marker_list_box.see(tkinter.END)
             self.marker_list.append(self.search_marker)
 
     def clear_marker_list(self):
         for marker in self.marker_list:
             self.map_widget.delete(marker)
 
-        self.marker_list_box.delete(0, tkinter.END)
         self.marker_list.clear()
         self.connect_marker()
 
@@ -117,8 +127,12 @@ class App(tkinter.Tk):
         if len(position_list) > 0:
             self.marker_path = self.map_widget.set_path(position_list)
 
-    def clear(self):
-        self.search_bar.delete(0, last=tkinter.END)
+    def clear_position(self):
+        self.search_position_bar.delete(0, last=tkinter.END)
+        self.map_widget.delete(self.search_marker)
+
+    def clear_fish(self):
+        self.search_fish_bar.delete(0, last=tkinter.END)
         self.map_widget.delete(self.search_marker)
 
     def on_closing(self, event=0):
